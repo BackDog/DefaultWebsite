1ca858a541b6501d904bbd4a0aa8bf1e431b58f1
Nguyễn Thanh Cảnh <canhnt1204@gmail.com>
2020-05-08 11:20:21 +0000

    Ghi chú Commit

---
+++ login/config.php
@@ -0,0 +1,7 @@
+<?php
+   define('DB_SERVER', 'localhost');
+   define('DB_USERNAME', 'rabbit');
+   define('DB_PASSWORD', '1204Thanhcanh@');
+   define('DB_DATABASE', 'db_rabbit');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
